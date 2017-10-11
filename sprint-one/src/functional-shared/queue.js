a78b4e52709b21b8e4b6903820113844c851977b
enriq018 <enriq018coding@gmail.com>
2017-10-11 15:47:17 -0700

    queue func shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,36 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.count = 0;
+  someInstance.front = 0;
+  extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
 
+
+
+var queueMethods = {};
+queueMethods.enqueue = function(value) {
+  this[this.count] = value;
+  this.count ++;
+};
+
+queueMethods.dequeue = function() {
+  var output = this[this.front];
+  delete this[this.front];
+  this.front++;
+  return output;
+};
+
+queueMethods.size = function() {
+  var total = this.count - this.front;
+  return total > 0 ? total : 0;
+};
 
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,25 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    count++;
+    storage[count] = value;
   };
 
   someInstance.dequeue = function() {
+    var keysArray = Object.keys(storage);
+    var minIndex = Math.min(...keysArray);
+    var minValue = storage[minIndex];
+    delete storage[minIndex];
+    return minValue;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
