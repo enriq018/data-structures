56de3b34dff696c0fd6d0aebed4794271b148950
enriq018 <enriq018coding@gmail.com>
2017-10-12 10:50:49 -0700

    linked list on contains

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,46 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+  //create container for new node 
+    var newNode = Node(value);
+    //conditinal check for head and tail
+    //if both null: set new node to both 
+    if (!list.head && !list.tail) {
+      list.head = newNode;
+      list.tail = newNode;
+    //if one exists
+    } else {
+    //get current tail, set its next prop to newnode
+      var current = list.tail;
+      current.next = newNode;  
+      //reassign list.tail to newnode
+      list.tail = newNode; 
+    } 
   };
 
   list.removeHead = function() {
+  //store list.head in a variable
+    var currentHead = list.head;
+    //check if the head and tail are equal
+    //if so, set both to null
+    //return stored head
+    if (list.head === list.tail) {
+      list.head = null;
+      list.tail = null;
+      return currentHead.value;
+    } else {
+      var newHead = currentHead.next;
+      currentHead.next = null;
+      list.head = newHead;
+      return currentHead.value;
+    } 
+
+  //else
+    
+    //store list.head.next
+    //list.head.next = null
+    //list.head = to stored list.head.next
+    //return stored list.head.next
   };
 
   list.contains = function(target) {
